753e92ebe6696e6952f5b856b8eda0b4b097cbd1
rkram5424 <rkram5424@gmail.com>
2014-05-05 00:17:56 -0500

    Butt ton of tiles. Started working on interface. Things coming together.

--- main.py
+++ main.py
@@ -1,5 +1,100 @@
-"""
-Main.
-"""
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
 
+#imports
+import random as r
+import sys
 from kivy.app import App
+from kivy.uix.button import Button
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+# Create both screens. Please note the root.manager.current: this is how
+# you can control the ScreenManager from kv. Each screen has by default a
+# property manager that gives you the instance of the ScreenManager used.
+Builder.load_string("""
+<JumblayaMenu>:
+	BoxLayout:
+		orientation: 'vertical'
+		padding: 50
+		Button:
+			text: 'New Bowl'
+			on_press: root.manager.current = 'bowls'
+		Button:
+			text: 'Quit'
+
+<BowlScreen>:
+	BoxLayout:
+		orientation: 'vertical'
+		padding: 50
+		Button:
+			text: 'Art & Literature'
+		Button:
+			text: 'Entertainment'
+		Button:
+			text: 'Geography'
+		Button:
+			text: 'History'
+		Button:
+			text: 'Science & Nature'
+		Button:
+			text: 'Misc'
+		Button:
+			text: 'Back to menu'
+			on_press: root.manager.current = 'menu'
+""")
+
+# Declare both screens
+class JumblayaMenu(Screen):
+	pass
+
+class BowlScreen(Screen):
+    pass
+
+
+# Create the screen manager
+sm = ScreenManager()
+sm.add_widget(JumblayaMenu(name='menu'))
+sm.add_widget(BowlScreen(name='bowls'))
+
+class JumblayaApp(App):
+	def build(self):
+		return sm
+
+class Jumblaya():
+	def __init__(self): 
+		word = sys.argv[1]
+		word = word.lower()
+		print (self.jumble_letters(word))
+		
+	def jumble_letters(self, word): # takes in word and spits out a jumbled mess.
+		word = word.replace(' ', '')
+		jumble_num = 0
+		new_num = 0
+		jumble_arr = []
+		if self.count_letters(word) > 1 & self.count_letters(word) < 10:
+			jumble_num = 10
+		elif self.count_letters(word) > 10 & self.count_letters(word) <= 20:
+			jumble_num = 20
+		new_num = jumble_num - self.count_letters(word)
+		for i in range(new_num):
+			word = word + self.random_letter()
+		jumble_arr = list(word)
+		r.shuffle(jumble_arr)
+		return jumble_arr
+		
+	def count_letters(self, answer):
+		non_letters = 0
+		answer = answer.lower()
+		letters = 'abcdefghijklmnopqrstuvwxyz'
+		for i in range(len(answer)):
+			if not(letters.contains(answer[i])):
+				non_letters += 1
+		return len(answer) - non_letters
+		
+	def random_letter(self):
+		letters = 'abcdefghijklmnopqrstuvwxyz'
+		return r.choice(letters)
+
+if __name__ == '__main__':
+    JumblayaApp().run()
