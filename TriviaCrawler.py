6338f4f533d6037b994427d411472a9d227218b6
rkram5424 <rkram5424@gmail.com>
2014-05-09 06:56:28 -0500

    Got argument-based game-screens to work... kinda. Time to add slots and tiles.

---
+++ TriviaCrawler.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+import urllib2
+
+class TriviaCrawler:
+	cat_array = ['ArtLiterature','Geography','Entertainment','History','ScienceNature','Misc']
+	root_url = 'http://www.randomtriviagenerator.com/'
+	num_ret_array = ['arts.php','geography.php','entertainment.php','history.php','science.php','misc.php']
+	add_array = ['question_a.php?q=','question_g.php?q=','question_e.php?q=','question_h.php?q=','question_s.php?q=','question_m.php?q=']
+	cat_nums = [0,0,0,0,0,0]
+	
+	def __init__(self):
+		self.crawl()
+	
+	def crawl(self):
+		for i in range(6):
+			response = urllib2.urlopen(self.root_url + self.num_ret_array[i])
+			page_source = response.read()
+			response.close()
+			self.cat_nums[i] = self.extract_between(page_source,'There are <b>','</b> questions', 1)
+			self.cat_nums[i] = int(self.cat_nums[i])
+			f = open(self.cat_array[i] + '.txt', 'w')
+			for j in range(self.cat_nums[i]):
+				question = ""
+				answer = ""
+				try:
+					response = urllib2.urlopen(self.root_url + self.add_array[i] + str(j),timeout=5)
+					page_source = response.read()
+					response.close()
+					question = self.extract_between(page_source,'<td align="left">','</td>',1)
+					answer = self.extract_between(page_source,'<td align="left">','</td>',2)
+					question = question.strip()
+					answer = answer.strip()
+					answer = self.refine_answer(answer)
+					answer = answer.upper()
+					if question != '' and answer != '':
+						print(question)
+						print(answer)
+						f.writelines(question + '|' + answer + '\n')
+				except:
+					continue
+			f.close()
+	
+	def refine_answer(self, answer):
+		if self.contains_digits(answer):
+			return ''
+		if self.count_letters(answer) > 20:
+			return ''
+		if self.count_letters(answer) < 4:
+			return ''
+		if answer[-2:] == '\'s':
+			answer = answer[:-2]
+		return answer
+		
+	def count_letters(self, answer):
+		non_letters = 0
+		answer = answer.lower()
+		letters = 'abcdefghijklmnopqrstuvwxyz'
+		for i in range(len(answer)):
+			if not answer[i] in letters:
+				non_letters += 1
+		return len(answer) - non_letters
+		
+	def contains_digits(self, answer):
+		return any(char.isdigit() for char in answer)
+	
+	def extract_between(self, text, sub1, sub2, nth):
+		"""
+		extract a substring from text between two given substrings sub1 (nth occurrence) and sub2 (nth occurrence) arguments are case sensitive
+		"""
+		# prevent sub2 from being ignored if it's not there
+		if sub2 not in text.split(sub1, nth)[-1]:
+			return None
+		return text.split(sub1, nth)[-1].split(sub2, nth)[0]
+
+if __name__ == '__main__':
+	TriviaCrawler()
+
--- main.py
+++ main.py
@@ -7,11 +7,46 @@ import sys
 import kivy
 from kivy.app import App
 from kivy.uix.button import Button
+from kivy.uix.label import Label
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 import Jumblaya
 
+class JumblayaMenu(Screen):
+	pass
+
+class BowlScreen(Screen):
+	def art(self):
+		gs = sm.current_screen
+		gs.set_bowl('ArtLiterature')
+	def ent(self):
+		gs = sm.current_screen
+		gs.set_bowl('Entertainment')
+	def geo(self):
+		gs = sm.current_screen
+		gs.set_bowl('Geography')
+	def his(self):
+		gs = sm.current_screen
+		gs.set_bowl('History')
+	def sci(self):
+		gs = sm.current_screen
+		gs.set_bowl('ScienceNature')
+	def msc(self):
+		gs = sm.current_screen
+		gs.set_bowl('Misc')
+
+class GameScreen(Screen):
+	hint = ""
+	def set_bowl(self, bowl):
+		jb = Jumblaya.Jumblaya(bowl)
+		self.hint = jb.return_data()[0]
+		l = Label(text=self.hint)
+		self.add_widget(l)
+
 Builder.load_string("""
+
+#:kivy 1.8.0
+
 <JumblayaMenu>:
 	BoxLayout:
 		orientation: 'vertical'
@@ -33,56 +68,39 @@ Builder.load_string("""
 		Button:
 			text: 'Art & Literature'
 			on_press: root.manager.current = 'game'
-			on_press: root.bowl = 'ArtLiterature'
+			on_release: root.art()
 		Button:
 			text: 'Entertainment'
 			on_press: root.manager.current = 'game'
-			on_press: root.bowl = 'Entertainment'
+			on_release: root.ent()
 		Button:
 			text: 'Geography'
 			on_press: root.manager.current = 'game'
-			on_press: root.bowl = 'Geography'
+			on_release: root.geo()
 		Button:
 			text: 'History'
 			on_press: root.manager.current = 'game'
-			on_press: root.bowl = 'History'
+			on_release: root.his()
 		Button:
 			text: 'Science & Nature'
 			on_press: root.manager.current = 'game'
-			on_press: root.bowl = 'ScienceNature'
+			on_release: root.sci()
 		Button:
 			text: 'Misc'
 			on_press: root.manager.current = 'game'
-			on_press: root.bowl = 'Misc'
+			on_release: root.msc()
 		Button:
 			text: 'Back to menu'
 			on_press: root.manager.current = 'menu'
-
-<GameScreen>:
-	FloatLayout:
-		Label:
-			text: root.hint
-
 """)
 
-class JumblayaMenu(Screen):
-	pass
-
-class BowlScreen(Screen):
-    pass
-
-class GameScreen(Screen):
-	jb = Jumblaya.Jumblaya('ArtLiterature') # How to set the param dynamically...
-	hint = jb.return_data()[0]
-	word = jb.return_data()[1]
-	jumble = jb.return_data()[2]
+sm = ScreenManager()
+sm.add_widget(JumblayaMenu(name='menu'))
+sm.add_widget(BowlScreen(name='bowls'))
+sm.add_widget(GameScreen(name='game'))
 
 class JumblayaApp(App):
 	def build(self):
-		sm = ScreenManager()
-		sm.add_widget(JumblayaMenu(name='menu'))
-		sm.add_widget(BowlScreen(name='bowls'))
-		sm.add_widget(GameScreen(name='game'))
 		return sm
 	
 if __name__ == '__main__':
